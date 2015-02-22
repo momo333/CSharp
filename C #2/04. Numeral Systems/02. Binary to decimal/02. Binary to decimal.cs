d8dfda19773f39eb72e84b70888c89d955b926a3
Maria Koseva <maria_2koseva@yahoo.com>
2015-02-22 12:57:41 +0200

    +
    repaired numerical systems

---
+++ C #2/04. Numeral Systems/02. Binary to decimal/02. Binary to decimal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+//•	Write a program to convert binary numbers to their decimal representation.
+class BinaryToDecimal
+{
+    static void Main()
+    {
+        Console.WriteLine("Please enter a number in binary representation: ");
+        double sum = 0;
+        int number = int.Parse(Console.ReadLine()); // binary number
+        int strn = number.ToString().Length; //how many digits has the number
+        for (int i = 0; i < strn; i++)
+        {
+            int lastDigit = number % 10; // get the last digit
+            sum = sum + lastDigit * (Math.Pow(2, i));
+            number = number / 10; //remove the last digit
+        }
+        Console.WriteLine("The numbe in decimal repreentation is: {0}", sum);
+    }
+}
+
---
+++ C #2/04. Numeral Systems/06. Binary to hexadecimal/06. Binary to hexadecimal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+//•	Write a program to convert binary numbers to hexadecimal numbers (directly).
+class BinaryToHexadecimal
+{
+    static string HexConverted(string strBinary)
+    {
+        string strHex = Convert.ToInt32(strBinary, 2).ToString("X");
+        return strHex;
+    }
+    static void Main()
+    {
+        Console.WriteLine("Please enter number in binary repreentation: ");
+        string binary = Console.ReadLine();
+        string hex = HexConverted(binary);
+        Console.WriteLine("The number in hexadecimal representation is: {0}", hex);
+    }
+}
+
+ 
